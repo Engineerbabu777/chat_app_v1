64bf5e3df5d72e5c7189752c1dde9b9689b63626
EngineerBabu777 <awaismumtaz0099@gmail.com>
2025-11-17 10:21:05 +0500

    add(props): added for CustomTextField widget

--- lib/core/common/custom_text_field.dart
+++ lib/core/common/custom_text_field.dart
@@ -1,7 +1,27 @@
 import 'package:flutter/material.dart';
 
 class CustomTextField extends StatelessWidget {
-  const CustomTextField({super.key});
+  final TextEditingController controller;
+  final String hintText;
+  final bool? obscureText;
+  final TextInputType? keyboardType;
+  final Widget? suffixIcon;
+  final Widget? prefxIcon;
+  final FocusNode? focusNode;
+
+  final String? Function(String?)? validator;
+
+  const CustomTextField({
+    super.key,
+    required this.controller,
+    required this.hintText,
+    this.obscureText = false,
+    this.keyboardType,
+    this.suffixIcon,
+    this.prefxIcon,
+    this.focusNode,
+    this.validator,
+  });
 
   @override
   Widget build(BuildContext context) {
