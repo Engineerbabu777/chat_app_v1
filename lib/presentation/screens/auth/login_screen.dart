87206e5c74a2ae7760856bd5d5275237efecc8d2
EngineerBabu777 <awaismumtaz0099@gmail.com>
2025-11-17 10:36:30 +0500

    add(login-screen): the top portion is bit ready

--- lib/presentation/screens/auth/login_screen.dart
+++ lib/presentation/screens/auth/login_screen.dart
@@ -1,3 +1,4 @@
+import 'package:chat_app/core/common/custom_text_field.dart';
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -8,28 +9,50 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordCOntroller = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Form(
-          child: Column(
-            children: [
-              SizedBox(height: 30),
-              Text(
-                "Welcome Back",
-                style: Theme.of(context).textTheme.headlineMedium?.copyWith(
-                  fontWeight: FontWeight.bold,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Form(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(height: 30),
+                Text(
+                  "Welcome Back",
+                  style: Theme.of(context).textTheme.headlineMedium?.copyWith(
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
-              ),
-              SizedBox(height: 10),
-              Text(
-                "Sign in to continue",
-                style: Theme.of(
-                  context,
-                ).textTheme.labelLarge?.copyWith(color: Colors.grey),
-              ),
-            ],
+                SizedBox(height: 10),
+                Text(
+                  "Sign in to continue",
+                  style: Theme.of(
+                    context,
+                  ).textTheme.labelLarge?.copyWith(color: Colors.grey),
+                ),
+                SizedBox(height: 30),
+
+                CustomTextField(
+                  controller: emailController,
+                  hintText: "Email",
+                  prefxIcon: Icon(Icons.email_outlined),
+                ),
+                SizedBox(height: 10),
+                CustomTextField(
+                  controller: passwordCOntroller,
+                  hintText: "Password",
+                  obscureText: true,
+                  prefxIcon: Icon(Icons.lock_outline),
+                  suffixIcon: Icon(Icons.visibility),
+                ),
+              ],
+            ),
           ),
         ),
       ),
