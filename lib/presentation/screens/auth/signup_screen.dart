7696c96be1d01f9243f173b8d20ff68ef53d14c6
EngineerBabu777 <awaismumtaz0099@gmail.com>
2025-11-17 11:11:06 +0500

    add(states): states has been added in SignUp screen

--- lib/presentation/screens/auth/signup_screen.dart
+++ lib/presentation/screens/auth/signup_screen.dart
@@ -8,8 +8,25 @@ class SignUpScreen extends StatefulWidget {
 }
 
 class _SignUpScreenState extends State<SignUpScreen> {
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController paswordController = TextEditingController();
+  final TextEditingController usernameController = TextEditingController();
+  final TextEditingController phoneController = TextEditingController();
+  final TextEditingController nameController = TextEditingController();
+
+  @override
+  void dispose() {
+    emailController.dispose();
+    paswordController.dispose();
+    usernameController.dispose();
+    phoneController.dispose();
+    nameController.dispose();
+
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold();
   }
 }
