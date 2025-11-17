87bc8272d5d6ba6043064547944013b08ebf6508
EngineerBabu777 <awaismumtaz0099@gmail.com>
2025-11-17 10:40:27 +0500

    add(CustomButton): the props has been added for this widget

--- lib/core/common/custom_button.dart
+++ lib/core/common/custom_button.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
 
 class CustomButton extends StatelessWidget {
-  const CustomButton({super.key});
+  final Function()? onPressed;
+  final String? text;
+  final Widget? child;
+
+  const CustomButton({super.key, this.onPressed, this.text, this.child})
+    : assert(
+        text != null || child != null,
+        'Einter text or child must be provided',
+      );
 
   @override
   Widget build(BuildContext context) {
