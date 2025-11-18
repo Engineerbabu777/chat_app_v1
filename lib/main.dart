ba090fbc4db4d643faa90ba5f54bde56556a9b82
EngineerBabu777 <awaismumtaz0099@gmail.com>
2025-11-18 21:55:51 +0500

    chore(firebase): firebase has been added configure setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:chat_app/config/theme/app_theme.dart';
 import 'package:chat_app/presentation/screens/auth/login_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
