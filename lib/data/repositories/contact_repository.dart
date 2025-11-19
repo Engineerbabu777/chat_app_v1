e619c717be21af8533d057bbd30d5b2d5b71ee5d
EngineerBabu777 <awaismumtaz0099@gmail.com>
2025-11-19 18:36:38 +0500

    add(requestContactsPermission): this new method has been added

--- lib/data/repositories/contact_repository.dart
+++ lib/data/repositories/contact_repository.dart
@@ -1,6 +1,11 @@
-
 import 'package:chat_app/data/services/base_repository.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_contacts/flutter_contacts.dart';
 
 class ContactRepository extends BaseRepository {
-  
+  String get currentUserId => FirebaseAuth.instance.currentUser!.uid ?? "";
+
+  Future<bool> requestContactsPermission() async {
+    return await FlutterContacts.requestPermission();
+  }
 }
