09b8f29093bdb42a5ff5c4a35156509d0855f42c
EngineerBabu777 <awaismumtaz0099@gmail.com>
2025-11-19 17:48:40 +0500

    add(floating-button): floating button has been added in the chats home screen

--- lib/presentation/home/home_screen.dart
+++ lib/presentation/home/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:chat_app/config/theme/app_theme.dart';
 import 'package:chat_app/data/services/service_locator.dart';
 import 'package:chat_app/logic/cubits/auth/auth_cubit.dart';
 import 'package:flutter/material.dart';
@@ -18,12 +19,18 @@ class HomeScreen extends StatelessWidget {
           ),
         ],
         actionsPadding: EdgeInsets.symmetric(horizontal: 20),
+        title: Text("Chats"),
       ),
       body: Center(
         child: Text(
           "User is AUTHENTICATED",
           style: TextStyle(fontWeight: FontWeight.w500, color: Colors.brown),
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: AppTheme.primaryColor,
+        child: const Icon(Icons.chat, color: Colors.white),
       ),
     );
   }
