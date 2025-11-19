f59dba01ed24ac5547bb261df7708d2273565512
EngineerBabu777 <awaismumtaz0099@gmail.com>
2025-11-19 19:52:41 +0500

    add(ChatRoomModel): all fields and constructor has been added

--- lib/data/models/chat_room_model.dart
+++ lib/data/models/chat_room_model.dart
@@ -1,5 +1,29 @@
-
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ChatRoomModel {
-  
+  final String id;
+  final List<String> participants;
+  final String? lastMessage;
+  final String? lastMessageSenderId;
+  final String? lastMessageTime;
+  final Timestamp? lastRead;
+  final Map<String, Timestamp>? lastReadTime;
+  final Map<String, String>? participantsName;
+  final bool isTyping;
+  final String? isTypingUserId;
+  final bool isCallActive;
+
+  ChatRoomModel({
+    required this.id,
+    required this.participants,
+    this.lastMessage,
+    this.lastMessageSenderId,
+    this.lastMessageTime,
+    this.lastRead,
+    this.lastReadTime,
+    this.participantsName,
+    this.isTyping = false,
+    this.isTypingUserId,
+    this.isCallActive = false,
+  });
 }
