5c551ec9dcb901d62ae1368d75a922cecec75db6
EngineerBabu777 <awaismumtaz0099@gmail.com>
2025-11-19 14:17:37 +0500

    add(signIn): this has been added new method in AuthRepository

--- lib/data/repositories/auth_repository.dart
+++ lib/data/repositories/auth_repository.dart
@@ -2,7 +2,6 @@ import 'dart:developer';
 
 import 'package:chat_app/data/models/user_model.dart';
 import 'package:chat_app/data/services/base_repository.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthRepository extends BaseRepository {
   Future<UserModel> signUp({
