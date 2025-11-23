20389688da7eb0ad498e3e31e07750f09d4c7a58
EngineerBabu777 <awaismumtaz0099@gmail.com>
2025-11-23 22:01:43 +0500

    add(getOrCreateChatRoom): this new method has been added in Chat reposityory

---
+++ lib/data/repositories/chat_repository.dart
@@ -0,0 +1,46 @@
+import 'package:chat_app/data/models/chat_room_model.dart';
+import 'package:chat_app/data/services/base_repository.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class ChatRepository extends BaseRepository {
+  CollectionReference get _chatRooms => firestore.collection("chatRooms");
+
+  Future<ChatRoomModel> getOrCreateChatRoom(
+    String currentUserId,
+    String otherUserId,
+  ) async {
+    final users = [currentUserId, otherUserId]..sort();
+
+    final roomId = users.join("_");
+
+    final roomDoc = await _chatRooms.doc(roomId).get();
+
+    if (roomDoc.exists) {
+      return ChatRoomModel.fromFirestore(roomDoc);
+    }
+
+    final currentUserData =
+        (await firestore.collection("users").doc(currentUserId).get()).data();
+
+    final otherUserData =
+        (await firestore.collection("users").doc(otherUserId).get()).data();
+
+    final participantsName = <String, String>{
+      currentUserId: currentUserData?["fullName"]?.toString() ?? "",
+      otherUserId: otherUserData?["fullName"]?.toString() ?? "",
+    };
+
+    final newRoom = ChatRoomModel(
+      id: roomId,
+      participants: users,
+      participantsName: participantsName,
+      lastReadTime: {
+        currentUserId: Timestamp.now(),
+        otherUserId: Timestamp.now(),
+      },
+    );
+
+    await _chatRooms.doc(roomId).set(newRoom.toMap());
+    return newRoom;
+  }
+}
