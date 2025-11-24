2110c7dbbe71e58ce40953d049ffcb53160cd3a6
EngineerBabu777 <awaismumtaz0099@gmail.com>
2025-11-24 15:51:30 +0500

    feat(APP_LIFE_CYCLE_OBSERVER): worked on this and added observ and bind it with our app

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:chat_app/config/theme/app_theme.dart';
+import 'package:chat_app/data/repositories/chat_repository.dart';
 import 'package:chat_app/data/services/service_locator.dart';
+import 'package:chat_app/logic/cubits/auth/auth_cubit.dart';
+import 'package:chat_app/logic/cubits/auth/auth_state.dart';
+import 'package:chat_app/logic/observer/app_life_cycle_observer.dart';
 import 'package:chat_app/presentation/screens/auth/login_screen.dart';
 import 'package:chat_app/router/app_router.dart';
 import 'package:flutter/material.dart';
@@ -10,8 +14,30 @@ void main() async {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  late AppLifeCycleObserver _lifecycleObserver;
+
+  @override
+  void initState() {
+    getIt<AuthCubit>().stream.listen((state) {
+      if (state.status == AuthStatus.authenticated && state.user != null) {
+        _lifecycleObserver = AppLifeCycleObserver(
+          userId: state.user!.uid,
+          chatRepository: getIt<ChatRepository>(),
+        );
+      }
+
+      WidgetsBinding.instance.addObserver(_lifecycleObserver);
+    });
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
