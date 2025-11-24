f87491f94c62f0b65671e3fa52f6395cf7f8e434
EngineerBabu777 <awaismumtaz0099@gmail.com>
2025-11-24 11:36:01 +0500

    add(ChatListTile): added new widget with props

---
+++ lib/presentation/widgets/chat_list_tile.dart
@@ -0,0 +1,20 @@
+import 'package:chat_app/data/models/chat_room_model.dart';
+import 'package:flutter/material.dart';
+
+class ChatListTile extends StatelessWidget {
+  final ChatRoomModel chat;
+  final String currentUserId;
+  final VoidCallback onTap;
+
+  const ChatListTile({
+    super.key,
+    required this.chat,
+    required this.currentUserId,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
