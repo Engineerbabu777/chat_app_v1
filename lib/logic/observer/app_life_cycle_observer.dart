231c903413e736bfdfdbbefb40aa7f863a18c5f1
EngineerBabu777 <awaismumtaz0099@gmail.com>
2025-11-24 15:47:35 +0500

    add(didChangeAppLifecycleState): this has been added new override methid in applifecycle management

--- lib/logic/observer/app_life_cycle_observer.dart
+++ lib/logic/observer/app_life_cycle_observer.dart
@@ -6,4 +6,23 @@ class AppLifeCycleObserver extends WidgetsBindingObserver {
   final ChatRepository chatRepository;
 
   AppLifeCycleObserver({required this.userId, required this.chatRepository});
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    super.didChangeAppLifecycleState(state);
+
+    switch (state) {
+      case AppLifecycleState.paused:
+      case AppLifecycleState.inactive:
+      case AppLifecycleState.detached:
+        chatRepository.updateOnlineStatus(userId, false);
+        break;
+      case AppLifecycleState.resumed:
+        chatRepository.updateOnlineStatus(userId, true);
+        break;
+
+      default:
+        break;
+    }
+  }
 }
