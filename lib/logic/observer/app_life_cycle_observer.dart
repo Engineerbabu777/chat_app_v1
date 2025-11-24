9d998f7839cda4e48acaab6dae91053a7ef1c433
EngineerBabu777 <awaismumtaz0099@gmail.com>
2025-11-24 15:44:42 +0500

    add(AppLifeCycleObserver): added member fields and extends as well

--- lib/logic/observer/app_life_cycle_observer.dart
+++ lib/logic/observer/app_life_cycle_observer.dart
@@ -1,3 +1,9 @@
+import 'package:chat_app/data/repositories/chat_repository.dart';
+import 'package:flutter/material.dart';
 
+class AppLifeCycleObserver extends WidgetsBindingObserver {
+  final String userId;
+  final ChatRepository chatRepository;
 
-class AppLifeCycleObserver {}
+  AppLifeCycleObserver({required this.userId, required this.chatRepository});
+}
